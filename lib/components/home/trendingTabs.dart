eb3c892579f4bcc0ae7e7a96706c89653e48e7e9
solomon-winner <solyalew3@gmail.com>
2024-08-03 12:06:24 +0300

    ...

---
+++ lib/components/home/trendingTabs.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class trendingTabs extends StatefulWidget {
+  const trendingTabs({super.key});
+
+  @override
+  State<trendingTabs> createState() => _trendingTabsState();
+}
+
+class _trendingTabsState extends State<trendingTabs> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold();
+  }
+}
