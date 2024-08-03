d9e8e2f1a9aae389590a31e0f2e5945e9352cc29
solomon-winner <solyalew3@gmail.com>
2024-08-03 22:07:09 +0300

    ...

---
+++ lib/components/home/bookList.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Booklist extends StatefulWidget {
+  const Booklist({super.key});
+
+  @override
+  State<Booklist> createState() => _BooklistState();
+}
+
+class _BooklistState extends State<Booklist> {
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [],
+    );
+  }
+}
