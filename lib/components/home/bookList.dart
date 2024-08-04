9f7cd12657e1e573e6b34964066d66ace2fa2a79
solomon-winner <solyalew3@gmail.com>
2024-08-04 17:20:59 +0300

    ...

--- lib/components/home/bookList.dart
+++ lib/components/home/bookList.dart
@@ -31,78 +31,103 @@ class _BooklistState extends State<Booklist> {
                   )
                 ]
               ),
-              child: Row(
-                
-               children: [
-                Padding(
-                  padding: const EdgeInsets.all(1.0),
-                  child: Container(
-                    // margin: EdgeInsets.only(top: 30),
-                    width: MediaQuery.sizeOf(context).width / 3,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.contain,
-                        image: AssetImage("assets/Born_a_crime.PNG"))
+              child: Stack(
+                children:[ 
+                  Row(
+                 children: [
+                  Padding(
+                    padding: const EdgeInsets.all(1.0),
+                    child: Container(
+                      // margin: EdgeInsets.only(top: 30),
+                      width: MediaQuery.sizeOf(context).width / 3,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.contain,
+                          image: AssetImage("assets/Born_a_crime.PNG"))
+                      ),
                     ),
                   ),
-                ),
-
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    child: Column(
-                      crossAxisAlignment:  CrossAxisAlignment.start,
-                      children: [
-                        Text("Born A Crime",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: BranaColor.BookTitleColor
-                        )
-                        ),
-                        SizedBox(height: 10,),
-                        Row(
-                          children: [
-                            Text("Author:",
-                          ),
-                            SizedBox(width: 5,),
-                            Text("Trevor Noah",
-                            ),
-                          ],
-                        ),
-                         SizedBox(height: 3,),
-                        Text("Biography"),
-                        SizedBox(height: 3,),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.star,
-                              color: BranaColor.StarColor ,
-                              ),
-                            Icon(Icons.star,
-                            color: BranaColor.StarColor,),
-                            Icon(Icons.star,
-                            color: BranaColor.StarColor,
-                            ),
-                            Icon(Icons.star,
-                            color: BranaColor.StarColor,
-                            ),
-                           Icon(
-                            Icons.star_half_outlined,
-                            color: Colors.amber,)
-                          ] 
-                        ),
-                        Text(
-                          "5 Books left",
+                
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment:  CrossAxisAlignment.start,
+                        children: [
+                          Text("Born A Crime",
                           style: TextStyle(
-                            fontSize: 12,
-                            color: BranaColor.LeftBookColor
+                            fontSize: 20,
+                            color: BranaColor.BookTitleColor
+                          )
                           ),
-                        )
+                          SizedBox(height: 10,),
+                          Row(
+                            children: [
+                              Text("Author:",
+                            ),
+                              SizedBox(width: 5,),
+                              Text("Trevor Noah",
+                              ),
+                            ],
+                          ),
+                           SizedBox(height: 3,),
+                          Text("Biography"),
+                          SizedBox(height: 3,),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                color: BranaColor.StarColor ,
+                                ),
+                              Icon(Icons.star,
+                              color: BranaColor.StarColor,),
+                              Icon(Icons.star,
+                              color: BranaColor.StarColor,
+                              ),
+                              Icon(Icons.star,
+                              color: BranaColor.StarColor,
+                              ),
+                             Icon(
+                              Icons.star_half_outlined,
+                              color: Colors.amber,)
+                            ] 
+                          ),
+                          Text(
+                            "5 Books left",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: BranaColor.LeftBookColor
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                  ] 
+                ),
+                Container(
+                  width: MediaQuery.sizeOf(context).width,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Icon(
+                          Icons.favorite_border,
+                          size: 25,
+                          color: BranaColor.AddLibrary,
+                        ),
+                          SizedBox(height: 10,),
+                           Icon(
+                          Icons.my_library_add_outlined,
+                          size: 25,
+                          color: BranaColor.AddLibrary,
+                        ),
                       ],
                     ),
                   ),
                 )
-                ] 
+              ]
               ),
             ),
           )
