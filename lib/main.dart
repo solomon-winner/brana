9d97fa4b977c1c7312aa0c751c710185dd13b812
solomon-winner <solyalew3@gmail.com>
2024-08-08 17:38:52 +0300

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:brana/Providers/book_provider.dart';
 import 'package:brana/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:brana/pages/home_page.dart';
+import 'package:provider/provider.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+    create: (context) => BookProvider(),
+    child: MyApp(),
+),
+);
 }
 
 class MyApp extends StatelessWidget {
