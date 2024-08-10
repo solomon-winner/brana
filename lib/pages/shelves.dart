3023855095e85be659c10e42cabb74023bef83d0
solomon-winner <solyalew3@gmail.com>
2024-08-10 22:58:10 +0300

    ...

---
+++ lib/pages/shelves.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Shelves extends StatefulWidget {
+  const Shelves({super.key});
+
+  @override
+  State<Shelves> createState() => _ShelvesState();
+}
+
+class _ShelvesState extends State<Shelves> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
