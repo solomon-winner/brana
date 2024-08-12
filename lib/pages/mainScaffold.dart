40bafe2cedebfe934f39c1a7286706ac575808f5
solomon-winner <solyalew3@gmail.com>
2024-08-12 12:39:11 +0300

    ...

--- lib/pages/mainScaffold.dart
+++ lib/pages/mainScaffold.dart
@@ -1,4 +1,8 @@
+import 'package:brana/components/home/bodyContainer.dart';
+import 'package:brana/utils/colors.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:brana/components/home/search_bar.dart';
 
 class MainScaffold extends StatefulWidget {
   final Widget body;
@@ -22,12 +26,112 @@ class _MainScaffoldState extends State<MainScaffold> {
         break;
       case 2:
         Navigator.pushReplacementNamed(context, "/favourites");
-      default:
+        break;
+      case 3:
+        Navigator.pushReplacementNamed(context, "/settings");
+        break;
+      case 4:
+        Navigator.pushReplacementNamed(context, "/profile");
+        break;
+      case 5:
+        Navigator.pushReplacementNamed(context, "/shelves");
+        break;
     }
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
-  }
+    return Scaffold(
+      backgroundColor: BranaColor.light_background,
+      appBar: AppBar(
+        leading: Icon(
+          Icons.menu,
+          color: BranaColor.WhiteColor,
+          ),
+        actions: [
+          Badge(
+            backgroundColor: BranaColor.BadgeBackground,
+            textColor: BranaColor.BadgeLabelColor,
+            label: Text("2"),
+            child: GestureDetector(
+              onTap:() => Navigator.pushReplacementNamed(context, "/shelves"),
+              child: Icon(Icons.shelves,
+              color: BranaColor.WhiteColor,
+              ),
+            ),
+          ),
+          SizedBox(width: 20,),
+          Icon(Icons.person,
+          color: BranaColor.WhiteColor,
+          ),
+          SizedBox(width: 10,)
+        ],
+        title: const Text(
+          "Brana Library",
+          style: TextStyle(
+            color: Colors.white
+          ),
+          ),
+        centerTitle: true,
+        backgroundColor: BranaColor.navigationColor,
+      ),
+      body:bodyContainer(),
+      
+      bottomNavigationBar: Container(
+        child: CurvedNavigationBar(
+          backgroundColor: BranaColor.light_background,
+          color:BranaColor.navigationColor,
+          height: 60,
+          animationDuration: Duration(milliseconds: 300),
+          onTap: (index) {
+            print(index);
+          },
+          items: [
+          Badge(
+            backgroundColor: BranaColor.BadgeBackground,
+            textColor: BranaColor.BadgeLabelColor,
+            label: Text("2"),
+            isLabelVisible: false,
+            child: GestureDetector(
+              onTap: () => Navigator.pushReplacementNamed(context, "/home"),
+              child: Icon(
+                Icons.home,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Badge(
+            backgroundColor: BranaColor.BadgeBackground,
+            textColor: BranaColor.BadgeLabelColor,
+            label: Text("2"),
+            child: GestureDetector(
+              onTap:() => Navigator.pushReplacementNamed(context, "/favourites"),
+              child: Icon(
+                Icons.favorite,
+                color: Colors.white,
+                ),
+            ),
+          ),
+          Badge(
+            backgroundColor: BranaColor.BadgeBackground,
+            textColor: BranaColor.BadgeLabelColor,
+            label: Text("2"),
+            isLabelVisible: false,
+            child: GestureDetector(
+             onTap:() => Navigator.pushReplacementNamed(context, "/settings"),
+
+              child: Icon(
+                Icons.settings,
+                color: Colors.white,
+                ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }  }
 }
+/////////////////////////////////
+///
+
+
