bcf69ead86067578f744b8cb72e49f320d827fdc
solomon-winner <solyalew3@gmail.com>
2024-08-12 12:04:47 +0300

    ...

--- lib/pages/mainScaffold.dart
+++ lib/pages/mainScaffold.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 
 class MainScaffold extends StatefulWidget {
-  const MainScaffold({super.key});
+  final Widget body;
+
+  MainScaffold({required this.body});
 
   @override
   State<MainScaffold> createState() => _MainScaffoldState();
 }
 
 class _MainScaffoldState extends State<MainScaffold> {
+
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    switch (index) {
+      case 0:
+        Navigator.pushReplacementNamed(context, "/home");
+        break;
+      case 2:
+        Navigator.pushReplacementNamed(context, "/favourites");
+      default:
+    }
+  }
+  
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
