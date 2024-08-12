9ed353f34cee2a3b0ffc4445c36eddff896cc20d
solomon-winner <solyalew3@gmail.com>
2024-08-12 11:45:43 +0300

    ...

---
+++ lib/pages/mainScaffold.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainScaffold extends StatefulWidget {
+  const MainScaffold({super.key});
+
+  @override
+  State<MainScaffold> createState() => _MainScaffoldState();
+}
+
+class _MainScaffoldState extends State<MainScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
