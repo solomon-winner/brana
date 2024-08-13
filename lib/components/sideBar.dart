b596a6adc58decdba7ecb696ab1989dcfbcc01d3
solomon-winner <solyalew3@gmail.com>
2024-08-13 14:03:32 +0300

    ...

--- lib/components/sideBar.dart
+++ lib/components/sideBar.dart
@@ -9,12 +9,32 @@ class SideBar extends StatefulWidget {
 
 class _SideBarState extends State<SideBar> {
   bool _isExpanded = false;
-  
+
+  void _toggleSidebar() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
       duration: Duration(milliseconds: 300),
-      width: 250,
+      width: _isExpanded ? 250 : 0,
+      child: Drawer(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('Home'),
+              trailing: IconButton(
+                icon: Icon(_isExpanded ?
+                Icons.arrow_back:
+                Icons.arrow_forward),
+                onPressed: _toggleSidebar,
+                ),
+              ),
+          ]
+            )
+        ),
     );
   }
 }
