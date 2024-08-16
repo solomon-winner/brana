4a6e8cb90b78f53673a915b09ff380f266f85fba
solomon-winner <solyalew3@gmail.com>
2024-08-16 21:48:35 +0300

    ...

---
+++ lib/components/details/bookDetail.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BookDetail extends StatelessWidget {
+  const BookDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
