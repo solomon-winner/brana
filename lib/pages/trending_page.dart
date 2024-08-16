78e9d2a97ef8889fe6e7fc8d72f83384032a8f32
solomon-winner <solyalew3@gmail.com>
2024-08-16 20:46:53 +0300

    ...

---
+++ lib/pages/trending_page.dart
@@ -0,0 +1,49 @@
+import 'package:brana/components/home/bookList.dart';
+import 'package:brana/components/home/search_bar.dart';
+import 'package:brana/components/home/trendingTabs.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class TrendinPage extends StatefulWidget {
+  const TrendinPage({super.key});
+
+  @override
+  State<TrendinPage> createState() => _TrendinPageState();
+}
+
+class _TrendinPageState extends State<TrendinPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+      ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: trendingTabs(),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        AnimatedSearchBar()
+                      ],
+                    ),
+                  )
+                 )
+                 
+              ],
+            ),
+            SizedBox(height: 20,),
+            Booklist()
+          ],
+        ),
+      );
+    
+  }
+}
