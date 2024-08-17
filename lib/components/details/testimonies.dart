12593717641791c0eaad8ba44021301f05faf377
solomon-winner <solyalew3@gmail.com>
2024-08-17 19:03:40 +0300

    ...

---
+++ lib/components/details/testimonies.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Testimonies extends StatefulWidget {
+  const Testimonies({super.key});
+
+  @override
+  State<Testimonies> createState() => _TestimoniesState();
+}
+
+class _TestimoniesState extends State<Testimonies> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
