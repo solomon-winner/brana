a5c17c189c5e96a56d100d542035f758cdb89171
solomon-winner <solyalew3@gmail.com>
2024-08-17 17:46:45 +0300

    ...

--- lib/components/details/booksByAuthor.dart
+++ lib/components/details/booksByAuthor.dart
@@ -14,62 +14,64 @@ class Booksbyauthor extends StatelessWidget {
           horizontal: 15.0,
           vertical: 10.0,
           ),
-      child: Row(
-        children: [
-          Container(
-                 decoration: BoxDecoration(
-        color: BranaColor.WhiteColor,
-        boxShadow: [
-          BoxShadow(
-            color: BranaColor.ShadowColor,
-            spreadRadius: -5.0,
-            blurRadius: 5.0,
-            offset: Offset(0.0,7.0)
-          )
-        ],
-        borderRadius: BorderRadius.circular(5),
-
-      ),
-            child: Column(
-              children: [
-                Container(
-                width: 100,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                    image: AssetImage('assets/Born_a_crime.PNG'),
-                    fit: BoxFit.contain,
+      child: SingleChildScrollView(
+        child: Row(
+          children: [
+            Container(
+                   decoration: BoxDecoration(
+          color: BranaColor.WhiteColor,
+          boxShadow: [
+            BoxShadow(
+              color: BranaColor.ShadowColor,
+              spreadRadius: -5.0,
+              blurRadius: 5.0,
+              offset: Offset(0.0,7.0)
+            )
+          ],
+          borderRadius: BorderRadius.circular(5),
+        
+        ),
+              child: Column(
+                children: [
+                  Container(
+                  width: 100,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: AssetImage('assets/Born_a_crime.PNG'),
+                      fit: BoxFit.contain,
+                    ),
                   ),
                 ),
+              SizedBox(height: 5),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Book Title',
+                    style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.black,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                  Text(
+                    '300 ETB',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
               ),
-            SizedBox(height: 5),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Book Title',
-                  style: TextStyle(
-                    fontSize: 17,
-                    color: Colors.black,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 1,
-                ),
-                Text(
-                  '300 ETB',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
+                ]
+              ),
             ),
-              ]
-            ),
-          ),
-
-        ],
+        
+          ],
+        ),
       ),
     );
   }
