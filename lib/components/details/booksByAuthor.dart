06766aae04067f61195f0b65d734669319d2d0bd
solomon-winner <solyalew3@gmail.com>
2024-08-17 17:12:50 +0300

    ...

---
+++ lib/components/details/booksByAuthor.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Booksbyauthor extends StatelessWidget {
+  const Booksbyauthor({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
