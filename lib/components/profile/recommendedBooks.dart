e42a42db7f80978e3b6d006df370a596d9d83859
solomon-winner <solyalew3@gmail.com>
2024-08-18 12:46:59 +0300

    ...

---
+++ lib/components/profile/recommendedBooks.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecommendedBooks extends StatefulWidget {
+  const RecommendedBooks({super.key});
+
+  @override
+  State<RecommendedBooks> createState() => _RecommendedBooksState();
+}
+
+class _RecommendedBooksState extends State<RecommendedBooks> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
