de3974e2d10fbd40a797767942b160797c16e88e
solomon-winner <solyalew3@gmail.com>
2024-08-18 11:58:15 +0300

    ...

--- lib/components/sideBar.dart
+++ lib/components/sideBar.dart
@@ -10,67 +10,60 @@ class SideBar extends StatefulWidget {
 }
 
 class _SideBarState extends State<SideBar> {
-
-  
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
       curve: Curves.easeInOutBack,
       duration: Duration(milliseconds: 300),
       decoration: BoxDecoration(
-        color: BranaColor.light_background,
-        boxShadow: [
-          BoxShadow(
-            color: BranaColor.ShadowColor,
-            blurRadius: 10,
-            spreadRadius: 1,
-            offset: Offset(0, 0),
-          ),
-        ],
-        borderRadius: BorderRadius.zero
-      ),
-      width:  250 ,
+          color: BranaColor.light_background,
+          boxShadow: [
+            BoxShadow(
+              color: BranaColor.ShadowColor,
+              blurRadius: 10,
+              spreadRadius: 1,
+              offset: Offset(0, 0),
+            ),
+          ],
+          borderRadius: BorderRadius.zero),
+      width: 250,
       child: Drawer(
-        shadowColor: BranaColor.ShadowColor,
-        backgroundColor: BranaColor.light_background,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.zero
-        ),
+          shadowColor: BranaColor.ShadowColor,
+          backgroundColor: BranaColor.light_background,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
           child: ListView(
             padding: EdgeInsets.zero,
             children: [
               UserAccountsDrawerHeader(
-                
-                    currentAccountPicture: CircleAvatar(
-                   backgroundColor: BranaColor.WhiteColor,
-                   child: ClipOval(child: Text("S",
-                   style: TextStyle(
-                    color: BranaColor.dark_background,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                   ),
-                   )),
-                 ),
-                 decoration: BoxDecoration(
-                   color: BranaColor.dark_background,
-                   image: DecorationImage(
-                     image: AssetImage("assets/Green.PNG"),
-                     fit: BoxFit.cover
-                   )
-                 ),
-                accountName: const Text("Solomon Yalew",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                )
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: BranaColor.WhiteColor,
+                  child: ClipOval(
+                      child: Text(
+                    "S",
+                    style: TextStyle(
+                      color: BranaColor.dark_background,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
                 ),
-                 accountEmail:  Text("solomonyalew664@gmail.com",
-                 style: TextStyle(
-                   color: BranaColor.WhiteColor,
+                decoration: BoxDecoration(
+                    color: BranaColor.dark_background,
+                    image: DecorationImage(
+                        image: AssetImage("assets/Green.PNG"),
+                        fit: BoxFit.cover)),
+                accountName: const Text("Solomon Yalew",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    )),
+                accountEmail: Text(
+                  "solomonyalew664@gmail.com",
+                  style: TextStyle(
+                    color: BranaColor.WhiteColor,
                     fontSize: 12,
-
-                 ),
-                 ),
+                  ),
+                ),
               ),
               ListTile(
                 leading: Icon(
@@ -78,40 +71,49 @@ class _SideBarState extends State<SideBar> {
                 ),
                 title: Text("Profile"),
                 onTap: () => Navigator.push(context,
-                MaterialPageRoute(
-                  builder: (context) { return Profile();})
-                ),
+                    MaterialPageRoute(builder: (context) {
+                  return Profile();
+                })),
               ),
-                            ListTile(
+              ListTile(
                 leading: Icon(
                   Icons.share,
                 ),
                 title: Text("Share"),
                 onTap: () => Navigator.push(context,
-                MaterialPageRoute(
-                  builder: (context) { return Profile();})
-                ),
+                    MaterialPageRoute(builder: (context) {
+                  return Profile();
+                })),
               ),
-                ListTile(
+              ListTile(
                 leading: Icon(
                   Icons.settings,
                 ),
                 title: Text("Setting"),
-                onTap: () => print(this),
+                onTap: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) {
+                  return Profile();
+                })),
               ),
-                            ListTile(
+              ListTile(
                 leading: Icon(
                   Icons.money,
                 ),
                 title: Text("Purchase History"),
-                onTap: () => print(this),
+                onTap: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) {
+                  return Profile();
+                })),
               ),
               ListTile(
                 leading: Icon(
                   Icons.notifications,
                 ),
                 title: Text("Notification"),
-                onTap: () => print(this),
+                onTap: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) {
+                  return Profile();
+                })),
               ),
               ListTile(
                 leading: Icon(
@@ -128,10 +130,7 @@ class _SideBarState extends State<SideBar> {
                 onTap: () => print(this),
               )
             ],
-          )
-
-
-        ),
+          )),
     );
   }
 }
