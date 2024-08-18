72b2319ffd2ac10defd97a3e5a27e0b648ee2f6f
solomon-winner <solyalew3@gmail.com>
2024-08-18 15:03:48 +0300

    ...

--- lib/components/profile/profileDetail.dart
+++ lib/components/profile/profileDetail.dart
@@ -8,22 +8,17 @@ class ProfileDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.sizeOf(context).width,
-      padding: const EdgeInsets.symmetric(
-        vertical: 15.0,
-        horizontal: 20.0
-      ),
+      padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
       decoration: BoxDecoration(
-        color: BranaColor.WhiteColor,
-        boxShadow: [
-          BoxShadow(
-            color: BranaColor.ShadowColor,
-            spreadRadius: -5.0,
-            blurRadius: 5.0,
-            offset: Offset(0.0, 7.0)
-          )
-        ],
-        borderRadius: BorderRadius.circular(20.0)
-      ),
+          color: BranaColor.WhiteColor,
+          boxShadow: [
+            BoxShadow(
+                color: BranaColor.ShadowColor,
+                spreadRadius: -5.0,
+                blurRadius: 5.0,
+                offset: Offset(0.0, 7.0))
+          ],
+          borderRadius: BorderRadius.circular(20.0)),
       child: Column(
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -32,70 +27,67 @@ class ProfileDetail extends StatelessWidget {
           Row(
             children: [
               Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                  "Solomon Yalew",
-                  style:TextStyle(
-                    fontSize: 23,
-                    fontWeight: FontWeight.bold,
-                    color: BranaColor.BookTitleColor
+                    "Solomon Yalew",
+                    style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        color: BranaColor.BookTitleColor),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 3,
                   ),
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 3,
-                          ),
-              Text(
-              "@sol_african",
-              style:TextStyle(
-                fontSize: 20,
-                color: BranaColor.BookTitleColor
-              ),
-              overflow: TextOverflow.ellipsis,
-              maxLines: 3,
-                      ),              
+                  Text(
+                    "@sol_african",
+                    style: TextStyle(
+                        fontSize: 20, color: BranaColor.BookTitleColor),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 3,
+                  ),
                 ],
               ),
               Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                        color: BranaColor.ShadowColor,
+                        spreadRadius: -5.0,
+                        blurRadius: 5.0,
+                        offset: Offset(0.0, 7.0))
+                  ],
+                ),
                 child: CircleAvatar(
                   backgroundColor: BranaColor.WhiteColor,
                   child: ClipOval(
                     child: Icon(
                       Icons.edit,
-                      
-                    ),),
+                      color: BranaColor.BookTitleColor,
+                    ),
+                  ),
                 ),
               )
             ],
           ),
-
-        SizedBox(height: 10,),  
-
-                  Text(
-          "Email: solomonyalew664@gmail.com",
-          style:TextStyle(
-            fontSize: 16,
-            color: BranaColor.BookTitleColor
+          SizedBox(
+            height: 10,
           ),
-          overflow: TextOverflow.ellipsis,
-          maxLines: 2,
-        ),
-           Text(
-          "phone No. : +251986780165",
-          style:TextStyle(
-            fontSize: 15,
-            color: BranaColor.BookTitleColor
+          Text(
+            "Email: solomonyalew664@gmail.com",
+            style: TextStyle(fontSize: 16, color: BranaColor.BookTitleColor),
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
           ),
-        ),
-           Text(
-          "Alternative phone No. : +251936187311",
-          style:TextStyle(
-            fontSize: 15,
-            color: BranaColor.BookTitleColor
+          Text(
+            "phone No. : +251986780165",
+            style: TextStyle(fontSize: 15, color: BranaColor.BookTitleColor),
           ),
-        ),
- 
+          Text(
+            "Alternative phone No. : +251936187311",
+            style: TextStyle(fontSize: 15, color: BranaColor.BookTitleColor),
+          ),
         ],
       ),
     );
