c683534bcebc6c16a28b525395fb397ccc562a69
solomon-winner <solyalew3@gmail.com>
2024-08-18 12:28:55 +0300

    ...

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,31 +1,71 @@
-import 'package:brana/utils/colors.dart';
-import 'package:flutter/material.dart';
+// import 'package:brana/components/details/bookDetail.dart';
+// import 'package:brana/components/details/bookImage.dart';
+// import 'package:brana/components/details/description.dart';
+// import 'package:brana/components/details/booksByAuthor.dart';
+// import 'package:brana/components/details/testimonies.dart';
+// import 'package:brana/models/books.dart';
+// import 'package:brana/utils/colors.dart';
+// import 'package:flutter/material.dart';
 
-class Profile extends StatefulWidget {
-  const Profile({super.key});
+// class Profile extends StatelessWidget {
 
-  @override
-  State<Profile> createState() => _ProfileState();
-}
-
-class _ProfileState extends State<Profile> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: BranaColor.light_background,
-      appBar: AppBar(
-        backgroundColor: BranaColor.AppBarTransparent,
-        centerTitle: true,
-        leading: Container(
-          color: BranaColor.WhiteColor,
-          child: IconButton(
-            onPressed: () => Navigator.of(context).pop(),
-            icon: Icon(
-              Icons.arrow_back,
-              color: BranaColor.dark_background,
-            )),
-        ),
-      ),
-    );
-  }
-}
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     return  Scaffold(
+//       backgroundColor: BranaColor.light_background,
+//       extendBodyBehindAppBar: true,
+//       appBar: AppBar(
+//        backgroundColor: BranaColor.AppBarTransparent,
+//        centerTitle: true,
+//         leading: Container(
+//           color: BranaColor.WhiteColor, // Set the background color to white
+//           child: IconButton(
+//             icon: Icon(
+//               Icons.arrow_back,
+//              color: BranaColor.dark_background),
+//             onPressed: () {
+//               Navigator.of(context).pop();
+//             },
+//           ),
+//         ),
+//       ),
+//       body: SingleChildScrollView(
+//         child: Column(
+//           children: [
+//             Container(
+//             height: 530,
+//             child: Stack(
+//               children: [
+//                 Positioned(
+            
+//                 // child: BookImage(),
+//                 ),
+//                  Positioned(
+//                   top: 230,
+//                   left: 0,
+//                   right: 0,
+//                   child: BookDetail(),
+//                   ),
+                
+//               ],
+//             ),
+//           ),
+//           SizedBox(height: 10,),
+//           Description(description: book.description,), 
+//           Booksbyauthor(),
+//           Testimonies(testimonies: [
+//             book.description, 
+//             book.description, 
+//             book.description,
+//             book.description,
+//             book.description,
+//             book.description,
+//             book.description,
+//             ]),
+//           ]
+//         )
+//       ),
+//     );
+//   }
+// }
