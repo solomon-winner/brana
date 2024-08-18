dfa79b1b354a8acdcb33c2c5eef34812de15dd46
solomon-winner <solyalew3@gmail.com>
2024-08-18 12:43:17 +0300

    ...

---
+++ lib/components/profile/favouriteCategories.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Favouritecategories extends StatefulWidget {
+  const Favouritecategories({super.key});
+
+  @override
+  State<Favouritecategories> createState() => _FavouritecategoriesState();
+}
+
+class _FavouritecategoriesState extends State<Favouritecategories> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
