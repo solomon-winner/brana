668d909830d7f0c11cf7343e170d70938d62d79f
solomon-winner <solyalew3@gmail.com>
2024-08-18 12:37:42 +0300

    ...

---
+++ lib/components/profile/aboutYou.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Aboutyou extends StatefulWidget {
+  const Aboutyou({super.key});
+
+  @override
+  State<Aboutyou> createState() => _AboutyouState();
+}
+
+class _AboutyouState extends State<Aboutyou> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
