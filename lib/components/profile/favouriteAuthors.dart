da6fb0e123aa59b6caadce8b8bef08864ccb7a2f
solomon-winner <solyalew3@gmail.com>
2024-08-18 12:44:18 +0300

    ...

---
+++ lib/components/profile/favouriteAuthors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Favouriteauthors extends StatefulWidget {
+  const Favouriteauthors({super.key});
+
+  @override
+  State<Favouriteauthors> createState() => _FavouriteauthorsState();
+}
+
+class _FavouriteauthorsState extends State<Favouriteauthors> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
