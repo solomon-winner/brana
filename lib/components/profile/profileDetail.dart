5a756f3567010d16ecb6a9a5406ba1231a001844
solomon-winner <solyalew3@gmail.com>
2024-08-18 14:23:23 +0300

    ...

--- lib/components/profile/profileDetail.dart
+++ lib/components/profile/profileDetail.dart
@@ -5,6 +5,8 @@ class ProfileDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: MediaQuery.sizeOf(context).width,
+    );
   }
 }
