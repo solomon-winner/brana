34d2521c0fc6586654d043c312d516df1b727c47
solomon-winner <solyalew3@gmail.com>
2024-08-19 11:02:19 +0300

    Component Loader...

---
+++ lib/utils/componentLoader.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Componentloader extends StatefulWidget {
+  const Componentloader({super.key});
+
+  @override
+  State<Componentloader> createState() => _ComponentloaderState();
+}
+
+class _ComponentloaderState extends State<Componentloader> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
