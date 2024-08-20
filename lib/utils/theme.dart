2b402e8cbc3c8394d0578f0e3c250c8375ef966c
solomon-winner <solyalew3@gmail.com>
2024-08-20 15:20:03 +0300

    Still About You Component...

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,3 +1,6 @@
-class AppThems {
-  
+import 'package:flutter/material.dart';
+
+class AppThemes {
+  static final ThemeData lightTeme = ThemeData();
+  static final ThemeData darkTheme = ThemeData();
 }
