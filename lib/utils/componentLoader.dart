7d7d4e9d81c0b1a1a04cc8c978f74d0fb71ea0bc
solomon-winner <solyalew3@gmail.com>
2024-08-21 15:24:44 +0300

    the server index...

--- lib/utils/componentLoader.dart
+++ lib/utils/componentLoader.dart
@@ -4,116 +4,116 @@ import 'package:flutter/material.dart';
 
 class Componentloader extends StatefulWidget {
   final List<String> list;
-    final String testimonier;
+  final String testimonier;
 
-   Componentloader({required this.list, this.testimonier = ''});
+  Componentloader({required this.list, this.testimonier = ''});
 
   @override
   State<Componentloader> createState() => _ComponentloaderState();
 }
 
 class _ComponentloaderState extends State<Componentloader> {
-    int _displayCount = 2;
+  int _displayCount = 2;
 
   void _loadMore() {
     setState(() {
       _displayCount += 2;
     });
   }
+
   void _Unload() {
     setState(() {
       _displayCount = 2;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     final displayedComponent = widget.list.take(_displayCount).toList();
-    final hasMore =  _displayCount < widget.list.length;
+    final hasMore = _displayCount < widget.list.length;
     return Container(
-
       child: Column(
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-
           ...displayedComponent.map((component) => Column(
-         mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,           
-            children: [
-              SeeMore(text: component,
-              testimonier: widget.testimonier == '' ?'':'Solomon Yalew: ',),
-          Divider(
-            color: BranaColor.DividerColor,
-            thickness: 1,
-          ),
-            ],
-          )),
-          hasMore ?
-            Container(
-              width: MediaQuery.sizeOf(context).width,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  TextButton(
-                    onPressed: _loadMore,
-
-                    child: Container(
-                      width: 105,
-                      child: Row(
-                        children: [
-                          Text(
-                            "Show More",
-                            style: TextStyle(
-                              color: BranaColor.BookTitleColor,
-                              fontSize: 15,
-                            ),
-                          ),
-                          Icon(
-                            Icons.arrow_drop_down,
-                            color: BranaColor.BookTitleColor,
-                          ),
-                        ],
-                      ),
-                    ),
+                  SeeMore(
+                    text: component,
+                    testimonier:
+                        widget.testimonier == '' ? '' : 'Solomon Yalew: ',
+                  ),
+                  Divider(
+                    color: BranaColor.DividerColor,
+                    thickness: 1,
                   ),
                 ],
-              ),
-            ) :
-
-                     Container(
-              width: MediaQuery.sizeOf(context).width,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton(
-                    onPressed: _Unload,
-                    child: Container(
-                      width: 105,
-                      child: Row(
-                        children: [
-                          Text(
-                            "Show Less",
-                            style: TextStyle(
-                              color: BranaColor.BookTitleColor,
-                              fontSize: 15,
-                            ),
+              )),
+          hasMore
+              ? Container(
+                  width: MediaQuery.sizeOf(context).width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed: _loadMore,
+                        child: Container(
+                          width: 105,
+                          child: Row(
+                            children: [
+                              Text(
+                                "Show More",
+                                style: TextStyle(
+                                  color: BranaColor.BookTitleColor,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Icon(
+                                Icons.arrow_drop_down,
+                                color: BranaColor.BookTitleColor,
+                              ),
+                            ],
                           ),
-                          Icon(
-                            Icons.arrow_drop_up,
-                            color: BranaColor.BookTitleColor,
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-            )  
-
+                )
+              : Container(
+                  width: MediaQuery.sizeOf(context).width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed: _Unload,
+                        child: Container(
+                          width: 105,
+                          child: Row(
+                            children: [
+                              Text(
+                                "Show Less",
+                                style: TextStyle(
+                                  color: BranaColor.BookTitleColor,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Icon(
+                                Icons.arrow_drop_up,
+                                color: BranaColor.BookTitleColor,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
         ],
       ),
     );
