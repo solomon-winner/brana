9fb20c3f6f90e6a207897f644c5912afd298bd97
solomon-winner <solyalew3@gmail.com>
2024-08-24 16:19:52 +0300

    ...

--- lib/services/book_service.dart
+++ lib/services/book_service.dart
@@ -6,7 +6,6 @@ import 'package:brana/utils/consts.dart';
 class BookService {
   Future<List<Book>>fetchBooks() async {
     final response = await http.get(Uri.parse('http://localhost:8000/Mobile/Flutter/brana/assets/info.json'));
-    print("${response} *******");
      if (response.statusCode == 200) {
       List<dynamic> data = json.decode(response.body);
       List<Book> books = data.map((book) =>Book.fromJson(book)).toList();
