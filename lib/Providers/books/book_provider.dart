1c51cad5a7f3d155a430c2c0f69173eb4d7f304e
solomon-winner <solyalew3@gmail.com>
2025-04-03 15:53:03 +0300

    ...users provider configuration...

--- lib/Providers/books/book_provider.dart
+++ lib/Providers/books/book_provider.dart
@@ -3,7 +3,6 @@ import 'package:brana/Providers/books/book_state.dart';
 import 'package:brana/data/datasources/book_remote_data_source.dart';
 import 'package:brana/data/repositories/book_repository/book_repository.dart';
 import 'package:brana/data/repositories/book_repository/book_repository_impl.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:brana/Providers/shared_preference.dart';
 import 'package:brana/Providers/dio_provider.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:brana/Blocs/bookBloc/book_bloc.dart';
-// import 'package:brana/Blocs/bookBloc/book_event.dart'; 
 import 'package:brana/pages/home_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:brana/Providers/shared_preference.dart';
-// import 'injection_controller.dart';
 
 void main() async{
-  // WidgetsFlutterBinding.ensureInitialized();
-  // init();
   WidgetsFlutterBinding.ensureInitialized();
   final SharedPreferences prefs = await SharedPreferences.getInstance();
   runApp(ProviderScope(
