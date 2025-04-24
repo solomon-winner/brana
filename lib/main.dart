23846fb6af30be3b301b4bc152adff3be033cad8
solomon-winner <solyalew3@gmail.com>
2025-04-24 20:40:34 +0300

    ...integrating login page in main file...

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:brana/Providers/dio_provider.dart';
+import 'package:brana/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:brana/pages/home_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:brana/Providers/shared_preference.dart';
 
@@ -8,20 +11,28 @@ void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   final SharedPreferences prefs = await SharedPreferences.getInstance();
 
+  final secureStorage = const FlutterSecureStorage();
+  final token = await secureStorage.read(key: 'auth_token');
+
   runApp(ProviderScope(
-    overrides: [sharedPreferencesProvider.overrideWithValue(prefs),],
-    child: MyApp()) );
+    overrides: [
+      secureStorageProvider.overrideWithValue(secureStorage),
+      sharedPreferencesProvider.overrideWithValue(prefs),],
+    child: MyApp(isLoggedIn: token != null)) );
 }
 
+
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isLoggedIn;
+  const MyApp({super.key, required this.isLoggedIn});
 
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
         debugShowCheckedModeBanner: false,
 
-        home: HomePage(),
+        home: isLoggedIn? HomePage(): LoginScreen(),
     );
   }
 }
