dff617537e3d3369e3c0a4aedecc203f89d22b7e
solomon-winner <solyalew3@gmail.com>
2025-05-28 15:11:03 +0300

    ...adding or removing data source class...

--- lib/data/datasources/book_remote_data_source.dart
+++ lib/data/datasources/book_remote_data_source.dart
@@ -1,12 +1,15 @@
-import 'dart:convert';
+// import 'dart:convert';
 import 'package:brana/utils/isSuccess.dart';
 import 'package:dio/dio.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 import 'package:brana/models/book_model/books.dart';
+import 'package:brana/models/user_collection_model/user_collection.dart';
 
 abstract class BookRemoteDataSource {
   Future<List<Book>> getBooks();
   Future<void> toggleBookLike(String bookId, String type);
+  Future<List<UserCollection>> getWishListItems();
+  Future<void> addOrRemoveWishList(String bookId, String type);
 }
 
 class BookRemoteDataSourceImpl implements BookRemoteDataSource {
@@ -57,21 +60,42 @@ class BookRemoteDataSourceImpl implements BookRemoteDataSource {
   }
 
   @override
-  Future<void> getWishListItems() async {
+  Future<List<UserCollection>> getWishListItems() async {
     try{
     final response = await dio.get('/wishlist');
 
     if (isSuccessStatus(response.statusCode)) {
       final data = response.data;
 
-      final List<dynamic> booksJson = data["data"];
-      
-      // return bookJson.map((json) => )
+      final List<dynamic> wishListJson = data["data"];
+
+      return wishListJson.map((json) =>  UserCollection.fromJson(json)).toList();
     } else {
-      throw Exception("Failed toload books");
+      throw Exception("Failed to load wishlists");
     }
   } catch (e) {
     throw Exception("Error fetching wishlists: $e");
   }
   }
+    @override
+    Future<void> addOrRemoveWishList(String bookId, String type) async{
+      try {
+        late Response response;
+
+        if (type == "add") {
+          response = await dio.post("/wishlist", data: {"bookId": bookId});
+        } else if (type == "remove") {
+          response = await dio.delete("/wishlist",data:{"bookId": bookId});
+        } else {
+          throw ArgumentError("Invalid type: $type. Use 'add' or 'remove'.");
+        }
+        if (!isSuccessStatus(response.statusCode)) {
+          throw Exception("Failed to add or remove a book!");
+        }
+      } catch (e) {
+        throw Exception("Error adding a book to wishlist: $e");
+      }
+    }
+  
+
 }
