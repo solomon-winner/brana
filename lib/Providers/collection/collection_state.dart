4cdab4008b0e8b567412d899fd39e18dc58de03c
solomon-winner <solyalew3@gmail.com>
2025-05-28 20:11:29 +0300

    ...creating collection state...

---
+++ lib/Providers/collection/collection_state.dart
@@ -0,0 +1,31 @@
+import 'package:brana/models/user_collection_model/user_collection.dart';
+
+class CollectionState {
+  final List<UserCollection> wishlist;
+  final List<UserCollection> recomended;
+  final List<UserCollection> saved;
+  final List<UserCollection> favourite
+
+
+  const CollectionState({
+    required this.wishlist,
+    required this.recomended,
+    required this.saved,
+    required this.favourite,
+  });
+
+  CollectionState copyWith({
+    List<UserCollection>? wishlist,
+    List<UserCollection>? recomended,
+    List<UserCollection>? saved,
+    List<UserCollection>? favourite,
+
+  }) {
+    return CollectionState(
+      wishlist: wishlist ?? this.wishlist,
+      recomended: recomended ?? this.recomended,
+      saved: saved ?? this.saved,
+      favourite: favourite ?? this.favourite,
+    );
+  }
+}
